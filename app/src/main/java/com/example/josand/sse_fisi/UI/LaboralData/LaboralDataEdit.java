e2427ca333ee6505786619d74fedd6ca05df95ee
Jose Andres <jace796@gmail.com>
2017-12-29 20:14:45 -0500

    Add LaboralDataEditView

---
+++ app/src/main/java/com/example/josand/sse_fisi/UI/LaboralData/LaboralDataEdit.java
@@ -0,0 +1,15 @@
+package com.example.josand.sse_fisi.UI.LaboralData;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.josand.sse_fisi.R;
+
+public class LaboralDataEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laboral_data_edit);
+    }
+}
--- app/src/main/java/com/example/josand/sse_fisi/UI/LaboralData/LaboralDataShow.java
+++ app/src/main/java/com/example/josand/sse_fisi/UI/LaboralData/LaboralDataShow.java
@@ -1,9 +1,12 @@
 package com.example.josand.sse_fisi.UI.LaboralData;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.example.josand.sse_fisi.Model.LaboralShowModel;
 import com.example.josand.sse_fisi.Model.PostgraduateShowModel;
@@ -25,6 +28,7 @@ public class LaboralDataShow extends AppCompatActivity {
         setContentView(R.layout.activity_laboral_data_show);
 
         List labList =new ArrayList();
+        FloatingActionButton fab= findViewById(R.id.fabLaboral);
 
         labList.add(new LaboralShowModel("Graña y Montero",
                 "Analista de Sistemas","2019-2021"));
@@ -37,5 +41,14 @@ public class LaboralDataShow extends AppCompatActivity {
 
         adapter =new LaboralDataShowAdapter(labList);
         recyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(),LaboralDataEdit.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
